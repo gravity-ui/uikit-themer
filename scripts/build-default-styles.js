d02c0a2c62d844516859c7ecfd8a52c44634542b
Daniil Gaponov <dagaponov@yandex-team.ru>
2025-07-07 11:57:13 +0200

    fix: build default styles

--- scripts/build-default-styles.js
+++ scripts/build-default-styles.js
@@ -3,6 +3,7 @@
 // This script is used to generate a default Gravity UIKit styles file.
 
 import {writeFile, mkdir, stat} from 'fs/promises';
+import {existsSync} from 'fs';
 import {fileURLToPath} from 'url';
 import {dirname, join} from 'path';
 
@@ -21,7 +22,7 @@ async function generateDefaultStyles() {
             forPreview: false,
         });
 
-        if (!(await stat(dirname(cssPath)))) {
+        if (!existsSync(dirname(cssPath))) {
             await mkdir(dirname(cssPath), {recursive: true});
         }
         await writeFile(cssPath, css, 'utf8');
